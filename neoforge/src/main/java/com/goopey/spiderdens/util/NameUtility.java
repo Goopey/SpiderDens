2350fdc6b6abc23adfe7ae2f4ae73e24d422b2c8
Paul Muraru <paulmuraru2004@gmail.com>
2024-08-09 16:39:44 -0400

    Add a getBlockName in NameUtility

--- neoforge/src/main/java/com/goopey/spiderdens/util/NameUtility.java
+++ neoforge/src/main/java/com/goopey/spiderdens/util/NameUtility.java
@@ -4,10 +4,11 @@ import com.goopey.spiderdens.SpiderDens;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class NameUtility {
   /**
-   * This function takes in an Item and extracts its name to then format
+   * This method takes in an Item and extracts its name to then format
    * it appropriately for registry stuff.
    * 
    * @param item the item whose name we will format
@@ -15,6 +16,18 @@ public class NameUtility {
    */
   public static String getItemName(Item item) {
     return BuiltInRegistries.ITEM.getKey(item)
-      .toString().replace(SpiderDens.MODID + ":", "");
+      .toString().replace(SpiderDens.MOD_ID + ":", "");
+  }
+
+  /**
+   * This method takes in a Block and extracts its name to then format 
+   * it appropriately for registry stuff.
+   * 
+   * @param block the block whose name we will format
+   * @return the block name as a string
+   */
+  public static String getBlockName(Block block) {
+    return BuiltInRegistries.BLOCK.getKey(block)
+      .toString().replace(SpiderDens.MOD_ID + ":", "");
   }
 }
