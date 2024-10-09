9cdb0e075880e0b8631f7253517b8e649af99d35
Paul Muraru <paulmuraru2004@gmail.com>
2024-10-09 19:10:43 -0400

    Add Attributes to RedSpider (it now spawns, yay)

--- neoforge/src/main/java/com/goopey/spiderdens/core/init/EntityInit.java
+++ neoforge/src/main/java/com/goopey/spiderdens/core/init/EntityInit.java
@@ -6,10 +6,18 @@ import com.goopey.spiderdens.entity.RedSpider;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
+@EventBusSubscriber(modid = SpiderDens.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class EntityInit {
   public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, SpiderDens.MOD_ID);
 
@@ -20,4 +28,14 @@ public class EntityInit {
   private static String prefix(String path) {
 		return SpiderDens.MOD_ID + path;
 	}
+
+  @SubscribeEvent
+  public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
+    event.put(EntityInit.RED_SPIDER.get(), RedSpider.createAttributes().build());
+  }
+
+  @SubscribeEvent
+  public static void registerEntitySpawn(SpawnPlacementRegisterEvent event) {
+    event.register(EntityInit.RED_SPIDER.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Monster::checkMonsterSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
+  }
 }
--- neoforge/src/main/java/com/goopey/spiderdens/entity/RedSpider.java
+++ neoforge/src/main/java/com/goopey/spiderdens/entity/RedSpider.java
@@ -1,11 +1,23 @@
 package com.goopey.spiderdens.entity;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Spider;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 
 public class RedSpider extends Spider {
   public RedSpider(EntityType<? extends RedSpider> entityType, Level level) {
     super(entityType, level);
   }
+
+  public static AttributeSupplier.Builder createAttributes() {
+		return Monster.createMonsterAttributes()
+      .add(Attributes.MOVEMENT_SPEED, (double) 0.3F)
+      .add(Attributes.FOLLOW_RANGE, 20.0D)
+      .add(Attributes.MAX_HEALTH, 16.0D)
+      .add(Attributes.ARMOR, 0.0D)
+      .add(Attributes.ATTACK_DAMAGE, 3.0D);
+	}
 }
